2112b3bf6f84d2f468d5791a32a6c5534f5082d5
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-05 21:22:19 +0200

    add arrays bills, tips, total

--- p2/js/script.js
+++ p2/js/script.js
@@ -30,3 +30,9 @@ function calcTip (billValue) {
     }
     return billValue * tipRate;
 }
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
