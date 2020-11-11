c58374d87f741cca875d75947ac1cf90ba0ecc18
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-11 20:28:52 +0200

    add function which convert array to string

---
+++ p5/js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+//Solving
+
+// 1) Перебрать массив
+// 2) Каждое новое значение добавить в строку
+// 3) Вернуть строку со значениями
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+function printForecast (arr) {
+    let resultString = "";
+    for (let i = 0; i < arr.length; i++) {
+        resultString += `... ${arr[i]}ºC in ${i+1} days `
+    }
+    console.log(resultString + "...");
+}
+
+printForecast(data1);
+printForecast(data2);
