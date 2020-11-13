03af9bfa47a12c1c4c7fc89af1c40f767f73cc31
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-13 13:19:01 +0200

    add functions for show and hidewindow in modal window prj

--- Modal-Window/js/script.js
+++ Modal-Window/js/script.js
@@ -1,1 +1,22 @@
 'use strict'
+const modalWindow = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const showModal = function () {
+    modalWindow.classList.remove('hidden');
+    modalOverlay.classList.remove('hidden');
+}
+
+const hideModal = function () {
+    modalWindow.classList.add('hidden');
+    modalOverlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', showModal)
+}
+
+btnCloseModal.addEventListener('click', hideModal)
+modalOverlay.addEventListener('click', hideModal)
