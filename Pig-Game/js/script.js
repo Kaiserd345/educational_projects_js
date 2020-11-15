bc9480991bff41caccccbafabb8a7291ca6032af
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-15 18:59:55 +0200

    add proj structure and eventListener to btnRoll

--- Pig-Game/js/script.js
+++ Pig-Game/js/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//Selecting Elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling Dice functionality
+btnRoll.addEventListener('click', function () {
+    //1.Generating random
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2.Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./img/dice-${dice}.png`;
+
+    //3. Check for rolled 1
+    if (dice !== 1) {
+        currentScore += dice;
+    } else {
+       // switch to next player
+
+    }
+})
