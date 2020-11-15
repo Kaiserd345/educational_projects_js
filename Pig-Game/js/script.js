ade1f6c86e2b02b1376247df2532ab1261ea0fea
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-15 23:52:42 +0200

    add switching player functionality

--- Pig-Game/js/script.js
+++ Pig-Game/js/script.js
@@ -1,15 +1,20 @@
 'use strict';
 
 //Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Starting conditions
 score0El.textContent = 0;
@@ -27,8 +32,16 @@ btnRoll.addEventListener('click', function () {
     //3. Check for rolled 1
     if (dice !== 1) {
         currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
     } else {
        // switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
-})
+});
+
+//Hold Functionality
