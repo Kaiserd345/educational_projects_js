2bd2e414d45a7e91e4c0e1a537bbd6d614afec0f
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-19 19:55:54 +0200

    add logic for all 7 cases

--- p6/js/script.js
+++ p6/js/script.js
@@ -66,3 +66,25 @@ const game = {
         team2: 6.5,
     },
 };
+//1.
+const [players1, players2] = game.players;
+//2.
+const [gk, ...fieldPlayers] = players1;
+//3.
+const allPlayers = [...players1, ...players2];
+//4.
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+//5.
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+//6.
+const printGoals = function (...players) {
+    console.log(`${players.length} goals scored.`)
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+//7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
