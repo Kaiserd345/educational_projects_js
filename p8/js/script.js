b117c59c05788112723f08801b4ecf4264a20424
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-21 22:44:21 +0200

    add my solution to project8

--- p8/js/script.js
+++ p8/js/script.js
@@ -2,12 +2,17 @@
 // Coding Challenge #3
 
 /*
-Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during
+the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football
+game has 90 minutes plus some extra time).
 
 1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event
+from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a
+game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45
+min) of the game, like this:
       [FIRST HALF] 17: ⚽️ GOAL
 
 GOOD LUCK 😀
@@ -26,3 +31,36 @@ const gameEvents = new Map([
     [80, '⚽️ GOAL'],
     [92, '🔶 Yellow card'],
 ]);
+
+//1
+const events = [... new Set(gameEvents.values())];
+console.log(events);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+function findAverageEventTime (map) {
+    // const timeEvents = [...map.keys()];
+    // let sum = 0;
+    // for (const time of timeEvents) {
+    //     sum += Number(time);
+    // }
+
+    const average = 90 / [...map.keys()].length;
+    console.log(`An event happened, on average, every ${average} minutes`);
+}
+
+findAverageEventTime(gameEvents);
+
+//4
+// [FIRST HALF] 17: ⚽️ GOAL
+for (const key of [...gameEvents.keys()]) {
+    if (key <= 45) {
+        console.log(`[FIRST HALF] ${key}: ${gameEvents.get(key)}`);
+    } else {
+        console.log(`[SECOND HALF] ${key}: ${gameEvents.get(key)}`);
+    }
+}
+
