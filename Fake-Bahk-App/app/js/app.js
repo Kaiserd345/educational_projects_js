9a9344fde04048d58e58e09494c4cac09c147d0f
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-27 17:11:33 +0200

    add login functionality

--- Fake-Bahk-App/app/js/app.js
+++ Fake-Bahk-App/app/js/app.js
@@ -5,28 +5,28 @@ const account1 = {
     owner: 'Dmytro Arkhypov',
     movements: [500, 450, -300, 6000, -550, -130, 60, 1300, 320],
     interestRate: 1.2, // %
-    pin: 1111,
+    password: 1111,
 };
 
 const account2 = {
     owner: 'Alexander Brin',
     movements: [3000, 7400, -150, -790, -4210, -1200, 8500, -30],
     interestRate: 1.5,
-    pin: 2222,
+    password: 2222,
 };
 
 const account3 = {
     owner: 'Barack Hussein Obama',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
-    pin: 3333,
+    password: 3333,
 };
 
 const account4 = {
     owner: 'Peter Mulinie',
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
-    pin: 4444,
+    password: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -50,7 +50,7 @@ const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
 const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
+const inputLoginPassword = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
@@ -70,28 +70,28 @@ const displayMovements = function (movements) {
         containerMovements.insertAdjacentHTML('afterbegin', html);
     });
 }
-displayMovements(account1.movements);
+
 
 const calcDisplayBalance = function (mov) {
     const balance = mov.reduce((accum, cur) => accum + cur, 0);
     labelBalance.textContent = `${balance} €`
 };
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-    const incomes = movements.filter(mov => mov > 0).reduce((accum, mov) => accum + mov, 0);
-    const out = movements.filter(mov => mov < 0).reduce((accum, mov) => accum + mov, 0);
-    const interest = movements.filter(mov => mov > 0).map(dep => dep * 0.15).reduce((accum, interest) => accum + interest, 0);
+
+const calcDisplaySummary = function (acc) {
+    const incomes = acc.movements.filter(mov => mov > 0).reduce((accum, mov) => accum + mov, 0);
+    const out = acc.movements.filter(mov => mov < 0).reduce((accum, mov) => accum + mov, 0);
+    const interest = acc.movements.filter(mov => mov > 0).map(dep => dep * 0.15).reduce((accum, interest) => accum + interest, 0);
 
     labelSumIn.textContent = `${incomes} €`;
     labelSumOut.textContent = `${Math.abs(out)} €`;
     labelSumInterest.textContent = `${interest} €`
 }
-calcDisplaySummary(account1.movements);
+
 
 const createUserNames = function (accs) {
     accs.forEach(acc => {
-        acc.userName = acc.owner
+        acc.username = acc.owner
             .toLowerCase()
             .split(' ')
             .map(name => name[0])
@@ -100,3 +100,28 @@ const createUserNames = function (accs) {
 };
 createUserNames(accounts);
 
+// Event handler
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+    if (currentAccount && currentAccount.password === Number(inputLoginPassword.value)) {
+        //Display UI and message
+        labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+        containerApp.style.opacity = 100;
+        //Clear inputs
+        inputLoginUsername.value = inputLoginPassword.value = '';
+        //Remove focuses from inputs
+        inputLoginPassword.blur();
+        inputLoginUsername.blur();
+        //Display movements
+        displayMovements(currentAccount.movements);
+        //Display Balance
+        calcDisplayBalance(currentAccount.movements);
+        //Display Summary
+        calcDisplaySummary(currentAccount);
+    }
+})
