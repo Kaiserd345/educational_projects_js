e19e403fe6700b78b6ba8337ff2dbcdbc749722b
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-30 00:18:21 +0200

    add sort functionality

--- Fake-Bahk-App/app/js/app.js
+++ Fake-Bahk-App/app/js/app.js
@@ -57,9 +57,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = '';
-    movements.forEach(function (mov, i) {
+
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function (mov, i) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
         const html = `
         <div class="movements__row">
@@ -129,6 +132,8 @@ btnLogin.addEventListener('click', function (e) {
         inputLoginUsername.blur();
 
         updateUI(currentAccount);
+    } else {
+        alert('User name or password is wrong!');
     }
 });
 
@@ -179,4 +184,11 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value = inputClosePin.value = '';
     //Hide UI
     containerApp.style.opacity = 0;
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+   e.preventDefault();
+   displayMovements(currentAccount.movements, !sorted);
+   sorted = !sorted;
 });
