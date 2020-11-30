14dc4dfd92387fd2ac4eb00448f7144192caee8a
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-30 22:34:06 +0200

    add currency detection function and impliment it

--- Fake-Bahk-App/app/js/app.js
+++ Fake-Bahk-App/app/js/app.js
@@ -17,7 +17,8 @@ const account1 = {
         '2020-08-01T10:51:36.790Z',
         '2020-08-01T10:53:36.790Z',
     ],
-    local: 'en-GB',
+    local: 'en-US',
+    currency: 'USD'
 };
 
 const account2 = {
@@ -36,6 +37,7 @@ const account2 = {
         '2020-07-26T12:01:20.894Z',
     ],
     local: 'ru-RU',
+    currency: 'EUR'
 };
 
 const accounts = [account1, account2];
@@ -68,6 +70,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 //Functions
 
+const formatCurrency = function (value, local, currency) {
+    return new Intl.NumberFormat(local, {
+        style: 'currency',
+        currency:  currency,
+    }).format(value);
+}
+
 const formatMovementDate = function (date, locale) {
     const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
     const daysPassed = calcDaysPassed(new Date(), date);
@@ -96,11 +105,13 @@ const displayMovements = function (acc, sort = false) {
         const date = new Date(acc.movementsDates[i]);
         const displayDate = formatMovementDate(date, acc.local);
 
+        const formattedMov = formatCurrency(mov, acc.local, acc.currency)
+
         const html = `
         <div class="movements__row">
             <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
             <div class="movements__date">${displayDate}</div>
-            <div class="movements__value">${mov.toFixed(2)} €</div>
+            <div class="movements__value">${formattedMov}</div>
         </div>
     `;
         containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -109,20 +120,26 @@ const displayMovements = function (acc, sort = false) {
 
 
 const calcDisplayBalance = function (acc) {
-    const balance = acc.movements.reduce((accum, cur) => accum + cur, 0);
-    acc.balance = balance;
-    labelBalance.textContent = `${acc.balance.toFixed(2)} €`
+    acc.balance = acc.movements.reduce((accum, cur) => accum + cur, 0);
+    labelBalance.textContent = formatCurrency(acc.balance, acc.local, acc.currency);
 };
 
 
 const calcDisplaySummary = function (acc) {
-    const incomes = acc.movements.filter(mov => mov > 0).reduce((accum, mov) => accum + mov, 0);
-    const out = acc.movements.filter(mov => mov < 0).reduce((accum, mov) => accum + mov, 0);
-    const interest = acc.movements.filter(mov => mov > 0).map(dep => dep * 0.15).reduce((accum, interest) => accum + interest, 0);
+    const incomes = acc.movements
+        .filter(mov => mov > 0)
+        .reduce((accum, mov) => accum + mov, 0);
+    labelSumIn.textContent = formatCurrency(incomes, acc.local, acc.currency);
 
-    labelSumIn.textContent = `${incomes.toFixed(2)} €`;
-    labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
-    labelSumInterest.textContent = `${interest.toFixed(2)} €`
+    const out = acc.movements
+        .filter(mov => mov < 0)
+        .reduce((accum, mov) => accum + mov, 0);
+    labelSumOut.textContent = formatCurrency(out, acc.local, acc.currency);
+
+    const interest = acc.movements
+        .filter(mov => mov > 0).map(dep => dep * 0.15)
+        .reduce((accum, interest) => accum + interest, 0);
+    labelSumInterest.textContent = formatCurrency(interest, acc.local, acc.currency);
 }
 
 
