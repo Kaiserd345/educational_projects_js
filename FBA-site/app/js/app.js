bb711afe134f892cb826036fc1aff765d7d4a3b4
Dmytro Arkhypov <dimonarh@gmail.com>
2020-12-02 21:42:16 +0200

    add cookie message

--- FBA-site/app/js/app.js
+++ FBA-site/app/js/app.js
@@ -1,11 +1,19 @@
 'use strict';
 
 // Modal window
+const header = document.querySelector('.header');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const message = document.createElement('div');
 
+//Cookie message
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+setTimeout(() => header.prepend(message),3000)
+
+//Modal Window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -17,8 +25,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
